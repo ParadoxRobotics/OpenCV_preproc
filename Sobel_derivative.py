bc1de90f3d87da08ce4084813b2e2520336a9c85
ParadoxRobotics <45688885+ParadoxRobotics@users.noreply.github.com>
2019-07-31 10:41:31 +0200

    Update Sobel_derivative.py

--- Sobel_derivative.py
+++ Sobel_derivative.py
@@ -8,7 +8,7 @@ src = cv2.GaussianBlur(img, (3, 3), 0)
 src = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
 
 grad_x = cv2.Sobel(src, cv2.CV_16S, 1, 0, ksize=3, scale=1, delta=0, borderType=cv2.BORDER_DEFAULT)
-grad_y = cv2.Sobel(src, cv2.CV_16S, 1, 0, ksize=3, scale=1, delta=0, borderType=cv2.BORDER_DEFAULT)
+grad_y = cv2.Sobel(src, cv2.CV_16S, 0, 1, ksize=3, scale=1, delta=0, borderType=cv2.BORDER_DEFAULT)
 #abs_grad_x = cv2.convertScaleAbs(grad_x)
 #abs_grad_y = cv2.convertScaleAbs(grad_y)
 
